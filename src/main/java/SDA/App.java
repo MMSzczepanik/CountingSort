f541862c17609066e4ad533762180d7d6c254ebf
Michal Szczepanik <micszczepanik@gmail.com>
2019-02-23 10:35:55 +0100

    Sorting with HashMap

--- src/main/java/SDA/App.java
+++ src/main/java/SDA/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
          int[] array = {1,0,2,5,2};
-         int[] finalArray = Sort.countingSort(array);
+         int[] finalArray = Sort.countingSortPlus(array);
 
          for(int i=0; i<array.length;i++){
              System.out.print(finalArray[i]);
--- src/main/java/SDA/Sort.java
+++ src/main/java/SDA/Sort.java
@@ -1,5 +1,8 @@
 package SDA;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sort {
 
     public static int[] countingSort(int[] array){
@@ -36,4 +39,44 @@ public class Sort {
         return finalArray;
 
     }
+
+    public static int[] countingSortPlus(int[] array) {
+
+        // najwieksza wartosc
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+
+        Map<Integer,Integer> mapa = new HashMap<>();
+        for(int i=0; i<array.length; i++){
+             if(mapa.get(array[i])==null){
+                 mapa.put(array[i],1);
+             }else{
+                 mapa.replace(array[i],mapa.get(array[i])+1);
+             }
+         }
+
+        int sum = 0;
+        for(Integer id : mapa.keySet()) {
+            sum += mapa.get(id);
+            mapa.replace(id,sum);
+        }
+
+        //finalna
+        int[] finalArray = new int[array.length];
+
+        for(int i=0; i<array.length; i++){
+            finalArray[mapa.get(array[i])-1] = array[i];
+            mapa.replace(array[i],mapa.get(array[i]) - 1);
+        }
+
+        return finalArray;
+
+    }
+
+
 }
